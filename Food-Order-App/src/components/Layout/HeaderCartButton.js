364d98f542883cbea4e4809d4de202c6e84ee42f
Angel Valentin <angelgvalentin7@gmail.com>
2022-06-01 14:10:58 -0500

    added cart sum logic to cartProvider

--- Food-Order-App/src/components/Layout/HeaderCartButton.js
+++ Food-Order-App/src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,25 @@
 import React from "react";
+import {useContext} from "react/cjs/react.development";
+import CartContext from "../../store/card-context";
+
+import CartProvider from "../../store/CartProvider";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+    const cartCtx = useContext(CartContext);
+
+    const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => { //check how 
+        return currentNumber + item.amount;
+    }, 0);
+
     return (
         <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
             <span>Your Cart</span>
-            <span className={classes.badge}>3</span>
+            <span className={classes.badge}>{numberOfCartItems}</span>
         </button>
     );
 };
