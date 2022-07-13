108aab2383d0a945997cb4895b2cb4260473eeb7
Angel Valentin <angelgvalentin7@gmail.com>
2022-07-13 16:23:58 -0500

    Done with 179 - using async/await and loading

--- Section 14- Sending HTTP requests/01-starting-project/src/App.js
+++ Section 14- Sending HTTP requests/01-starting-project/src/App.js
@@ -5,23 +5,24 @@ import "./App.css";
 
 function App() {
     const [movies, setMovies] = useState([]);
-    function fetchMoviesHandler() {
-        fetch("https://swapi.dev/api/films/")
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                const transformedMovies = data.results.map((movieData) => {
-                    return {
-                        id: movieData.episode_id,
-                        title: movieData.title,
-                        openingText: movieData.opening_crawl,
-                        releaseDate: movieData.release_date,
-                    };
-                });
-                setMovies(transformedMovies);
-            });
-    }
+    const [isloading, setIsLoading] = useState(false);
+
+    const fetchMoviesHandler = async () => {
+        setIsLoading(true);
+        const response = await fetch("https://swapi.dev/api/films/");
+        const data = await response.json();
+
+        const transformedMovies = data.results.map((movieData) => {
+            return {
+                id: movieData.episode_id,
+                title: movieData.title,
+                openingText: movieData.opening_crawl,
+                releaseDate: movieData.release_date,
+            };
+        });
+        setMovies(transformedMovies);
+        setIsLoading(false);
+    };
 
     return (
         <React.Fragment>
@@ -29,7 +30,12 @@ function App() {
                 <button onClick={fetchMoviesHandler}>Fetch Movies</button>
             </section>
             <section>
-                <MoviesList movies={movies} />
+                {!isloading && movies.length > 0 && (
+                    <MoviesList movies={movies} />
+                )}
+                {isloading && <p>Loading...</p>}
+                {!isloading && movies.length == 0 && <p>Found no movies. </p>}
+                {isloading && <span class="loader"></span>}
             </section>
         </React.Fragment>
     );
