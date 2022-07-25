875994581768ee7f2eb22f2c51fdbb69a8358d95
Angel Valentin <angelgvalentin7@gmail.com>
2022-07-25 11:22:30 -0500

    Done with 237 -Counter now works

--- Section 18 - Diving into Redux/232 - starting-project/01-starting-project/src/components/Counter.js
+++ Section 18 - Diving into Redux/232 - starting-project/01-starting-project/src/components/Counter.js
@@ -1,9 +1,19 @@
-import {useSelector, connect} from "react-redux";
+import {useSelector, connect, useDispatch} from "react-redux";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
+    const dispatch = useDispatch();
     const counter = useSelector((state) => state.counter);
     // when we use useSelector, react will automagicaly setup a subscription to the redux-store for this component. that means it will be updated an recieve the latest counter everytime the counter changes in the redux store. It will cause this componenet to be re evaluated to keep it updated.
+
+    const incrementHandler = () => {
+        //an action is an object with a type property
+        dispatch({type: "increment"});
+    };
+
+    const decrementHandler = () => {
+        dispatch({type: "decrement"});
+    };
 
     const toggleCounterHandler = () => {};
 
@@ -11,6 +21,11 @@ const Counter = () => {
         <main className={classes.counter}>
             <h1>Redux Counter</h1>
             <div className={classes.value}>-- {counter} --</div>
+            <div>
+                <button onClick={incrementHandler}>Increment </button>
+                <button onClick={decrementHandler}>Decrement</button>
+            </div>
+            <div></div>
             <button onClick={toggleCounterHandler}>Toggle Counter</button>
         </main>
     );
