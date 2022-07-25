a9f8ee55b542894c1ccd25a702d0636ba10a1757
Angel Valentin <angelgvalentin7@gmail.com>
2022-07-25 14:47:27 -0500

    Done with 237 - Redux in class based components

--- Section 18 - Diving into Redux/232 - starting-project/01-starting-project/src/components/Counter.js
+++ Section 18 - Diving into Redux/232 - starting-project/01-starting-project/src/components/Counter.js
@@ -1,4 +1,5 @@
-import {useSelector, connect, useDispatch} from "react-redux";
+import {Component} from "react";
+import {useSelector, useDispatch} from "react-redux";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
@@ -30,5 +31,58 @@ const Counter = () => {
         </main>
     );
 };
+
+// class Counter extends Component {
+//     incrementHandler = () => {
+//         this.props.increment();
+//     };
+
+//     decrementHandler = () => {
+//         this.props.decrement();
+//     };
+
+//     toggleCounterHandler = () => {};
+
+//     render() {
+//         return (
+//             <main className={classes.counter}>
+//                 <h1>Redux Counter</h1>
+//                 <div className={classes.value}>-- {this.props.counter} --</div>
+//                 <div>
+//                     <button onClick={this.incrementHandler.bind(this)}>
+//                         Increment{" "}
+//                     </button>
+//                     <button onClick={this.decrementHandler.bind(this)}>
+//                         Decrement
+//                     </button>
+//                 </div>
+//                 <div></div>
+//                 <button onClick={this.toggleCounterHandler}>
+//                     Toggle Counter
+//                 </button>
+//             </main>
+//         );
+//     }
+// }
+
+// //connect wants two arguments. '
+// //the first is a function that maps redux state to props that are then recieved in the component.
+
+// const mapStateToProps = (state) => {
+//     return {
+//         counter: state.counter,
+//     };
+// };
+
+// const mapDispatchToProps = (dispatch) => {
+//     return {
+//         increment: () => dispatch({type: "increment"}),
+//         decrement: () => dispatch({type: "decrement"}),
+//     };
+// };
+// the second paramter that connect recieves is called mapDispatchToProps
+
+//connect will also setup a subscription like useSelector and useDispatch do
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
 
 export default Counter;
