8ab8320310f39b55088427aa61d056c6c091fd29
Angel Valentin <angelgvalentin7@gmail.com>
2022-08-12 15:47:13 -0500

    Cart add and remove buttons working

--- Section 19  - Advanced Redux/Starting Project/01-starting-project/src/components/Cart/CartButton.js
+++ Section 19  - Advanced Redux/Starting Project/01-starting-project/src/components/Cart/CartButton.js
@@ -1,4 +1,5 @@
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+
 import {uiActions} from "../../store/ui-slice";
 import classes from "./CartButton.module.css";
 
@@ -9,10 +10,12 @@ const CartButton = (props) => {
         dispatch(uiActions.toggle());
     };
 
+    const cartQuantity = useSelector((state) => state.cart.totalQuantity);
+
     return (
         <button onClick={toggleCartHandler} className={classes.button}>
             <span>My Cart</span>
-            <span className={classes.badge}>1</span>
+            <span className={classes.badge}>{cartQuantity}</span>
         </button>
     );
 };
